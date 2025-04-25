3d0e039d51d930a9dd03982d99942938f2029603
HaojieChen <923253757@qq.com>
2025-04-25 23:24:41 +0800

    update

--- gulimall-product/src/main/java/com/hojay/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/hojay/gulimall/product/GulimallProductApplication.java
@@ -4,12 +4,18 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.function.Consumer;
+
 @MapperScan("com.hojay.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
     public static void main(String[] args) {
+        Consumer<String> s = (String string) -> {
+            System.out.println(string);
+        };
         SpringApplication.run(GulimallProductApplication.class, args);
+
     }
 
 }
--- gulimall-product/src/main/java/com/hojay/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/hojay/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,10 @@
 package com.hojay.gulimall.product.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -24,6 +28,18 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        // 1. 查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        // 2. 组装成父子树形结构
+        //   (1) 招待哦所有的一级分类
+
+
+        return entities;
     }
 
 }
